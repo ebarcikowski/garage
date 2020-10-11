28185debc53290b887531230f48e4224b5b36563
Elliott Barcikowski <ebarcikowski@gmail.com>
2020-10-11 12:34:50 -0600

    Added flask app for deployment.

---
+++ garage/app.py
@@ -0,0 +1,23 @@
+from garage import nn
+import flask
+
+app = flask.Flask(__name__)
+
+model = nn.get_linear_model(warm_start=True)
+cap = nn.Capture()
+
+
+@app.route('/', methods=["GET", "POST"])
+def predict():
+    data = {"closed": False}
+
+    gen = model.predict(cap.make_pred_fn)
+    result = list(gen)[0]
+
+    if result['class_ids'] == 0:
+        data['closed'] = True
+    return flask.jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
