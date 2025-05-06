245b7119bf16333ad794bbb6867b462c4870df3b
Elliott Barcikowski <ebarcikowski@gmail.com>
2025-05-06 15:57:14 -0600

    Cleaned up non tf models.

--- garage/svm.py
+++ garage/svm.py
@@ -1,6 +1,10 @@
+"""Non-tensorflow models that I made along the way.
+
+This is mostly play and hasn't been tested in a long time. These
+models should work though.
+"""
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import sklearn.svm
 
 
@@ -14,14 +18,15 @@ SLICE_Y = slice(UPPER_LEFT[1], LOWER_RIGHT[1])
 
 
 class SVM:
+    """SVM based classifier"""
     CLOSED_IMGS = [
-        "data/closed_001.jpg",
-        "data/closed_002.jpg",
-        "data/closed_003.jpg"
+        "data/closed/closed_001.jpg",
+        "data/closed/closed_002.jpg",
+        "data/closed/closed_003.jpg"
     ]
     OPEN_IMGS = [
-        "data/open_001.jpg"
-        "data/open_002.jpg"
+        "data/open/open_001.jpg"
+        "data/open/open_002.jpg"
     ]
     UPPER_LEFT = (900, 40)
     LOWER_RIGHT = (1200, 380)
@@ -58,7 +63,12 @@ class SVM:
 
         return np.array(img_data)
 
-class Detector:
+class _Detector:
+    """
+    A OpenCV based detector
+
+    May work but hasn't been tested in a long time.
+    """
     CLOSED_IMG = "closed3.jpg"
     OPEN_IMG = "open.jpg"
     UPPER_LEFT = (900, 40)
@@ -108,78 +118,8 @@ class Detector:
         print([m.distance for m in matches[:5]])
         img_comp = cv2.drawMatches(self.ref_image, ref_kps,
                                    img, kps, matches[:5], out)
-        import matplotlib.pyplot as plt
-        plt.imshow(img_comp)
-        plt.show()
+        # import matplotlib.pyplot as plt
+        # plt.imshow(img_comp)
+        # plt.show()
 
         return matches
-
-
-def noise_sample(img_fn):
-    img = cv2.imread(img_fn, 0)
-    detector = Detector()
-    diff_img = detector.is_closed(img)
-    print('{}'.format(np.sum(diff_img)))
-    cv2.imshow("", diff_img)
-    cv2.waitKey(0)
-
-
-def test_matcher():
-    open_img = cv2.imread(Detector.OPEN_IMG)
-    closed_img = cv2.imread("closed3.jpg")
-
-    detector = Detector()
-    orb = cv2.ORB_create()
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    ref_kps, ref_desc = orb.detectAndCompute(self.ref_image, None)
-    img = img[self.slice_y, self.slice_x]
-    kps, desc = orb.detectAndCompute(img, None)
-    matches = bf.match(ref_desc, desc)
-
-    matches1 = detector.matcher(open_img)
-    matches2 = detector.matcher(closed_img)
-    img_match = cv2.drawMatches(
-        open_img,
-
-    )
-    return (
-
-        open_img,
-        closed_img
-    )
-
-
-class Capture:
-
-    def __init__(self, url=CAMERA_URL):
-        self.url = CAMERA_URL
-        self.cap = cv2.VideoCapture()
-        assert self.cap.open(self.url) is True
-
-    def get_image(self):
-        rc, img_color = self.cap.read()
-        img = cv2.cvtColor(img_color, cv2.COLOR_RGB2GRAY)
-        return img
-
-
-if __name__ == '__main__':
-    cap = Capture()
-    detector = Detector()
-
-    img = cap.get_image()
-    svm = SVM()
-
-    print(svm.predict(img))
-    # assert svm.predict(img) == 0, "Garage is open!"
-    # img = cap.get_image()
-    #
-   #  plt.imshow(img)
-   #  plt.show()
-
-
-    # is_closed = detector.is_closed(img)
-    # print(is_closed)
-    # detector.matcher(img)
-    # matcher1, matcher2, img1, img2 = test_matcher()
-    # if not is_closed:
-    #     raise Exception("Garage is open")
