5eb4ca7fe89c6a8e231cb7d1a8717b21699cfd65
Elliott Barcikowski <ebarcikowski@gmail.com>
2025-05-06 16:05:41 -0600

    comment util script.

--- bin/get_images.sh
+++ bin/get_images.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # API call to get JPEG images from ZM.
+# This a mini tool used to get some real life image without having
+# to removed the URL all the time.
 wget "http://localhost:8080/zm/cgi-bin/zms?mode=single&monitor=1"  -O foo.jpg
